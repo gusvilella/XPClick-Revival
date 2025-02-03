b156f5f73842db9abe85c0eb00e7b4845ab17065
Gustavo Attisano Vilella <gustavovilella38@gmail.com>
2025-02-03 02:49:00 -0300

    Atualizando xp-click.user.js: melhorias na detecção de cliques e suporte ao Google

--- xp-click.user.js
+++ xp-click.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         XPClick Revival
 // @namespace    http://tampermonkey.net/
-// @version      1.4
-// @description  Reproduz o som de clique clássico do Windows XP em qualquer página da web.
+// @version      1.9
+// @description  Reproduz o som de clique clássico do Windows XP em páginas comuns da web.
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
@@ -13,17 +13,36 @@
     console.log("XPClick Revival está rodando!");
 
     document.addEventListener('click', function(event) {
+        const tag = event.target.tagName.toLowerCase();
+
+        // Lista de elementos que fazem sentido ter som
+        const elementosInterativos = ['a', 'button', 'input', 'select', 'textarea', 'label'];
+
+        // Detecta cliques em links e botões
+        if (elementosInterativos.includes(tag)) {
+            tocarSom();
+            return;
+        }
+
+        // Corrige cliques nos resultados do Google (links dentro de divs)
+        if (event.target.closest('.tF2Cxc, .yuRUbf, .BVG0Nb, .rc')) {
+            tocarSom();
+            return;
+        }
+
+    }, true);
+
+    function tocarSom() {
         console.log("Clique detectado!");
 
         const audio = new Audio('https://gusvilella.github.io/XPClick-Revival/xp-click.wav');
         audio.volume = 1.0;
         audio.currentTime = 0;
 
-        // Força o áudio a rodar como fizemos no Console
         audio.play().then(() => {
             console.log("Som tocando!");
         }).catch(e => {
             console.error("Erro ao tocar o som:", e);
         });
-    }, true);
+    }
 })();
